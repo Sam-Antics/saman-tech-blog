f8b113bca9755ddb9439fed056f7d9afb642ddf0
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-08-08 01:28:30 -0500

    session logic

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -41,7 +41,15 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -67,10 +75,28 @@ router.post('/login', (req, res) => {
           res.status(400).json({ message: 'Incorrect password. '});
           return;
         }
-        
-        res.json({ user: dbUserData, message: 'You are now logged in.' });
+
+        req.session.save(() => {
+          // declare session variables
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+
+          res.json({ user: dbUserData, message: 'You are now logged in.' });
+        });
       });  
     });
+
+// logout to destroy session
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+    res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 // UPDATE existing user (api/users/1)
 router.put('/:id', (req, res) => {
